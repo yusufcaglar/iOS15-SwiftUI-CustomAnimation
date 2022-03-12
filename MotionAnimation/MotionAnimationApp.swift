e1941ab3cb1ff327ca2d7eed412536bd04af89d1
Yusuf Çağlar <faf7faf7@gmail.com>
2022-03-12 15:28:37 +0300

    Initial Commit

---
+++ MotionAnimation/MotionAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionAnimationApp.swift
+//  MotionAnimation
+//
+//  Created by Yusuf Çağlar on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MotionAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
